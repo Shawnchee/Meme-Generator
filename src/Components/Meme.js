313eef430a011b5ec5d0881a0ee83a67acf4d96d
Shawnchee <aykaychee2@gmail.com>
2023-11-26 00:17:19 +0800

    Edited form using grid instead of flex

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -2,19 +2,19 @@ import React from 'react'
 import pictureIcon from "./Icons/picture.svg"
 
 export default function Meme(){
+    function handleClick () {
+        alert('I was clicked')
+    }
     return (
-        <div>
-            <div className='input-container'>
-            <input className='input-1'></input>
-            <input className='input-2'></input>
-            </div>
-            <div className='button-container'>
-            <button className='button'>Get a new meme image
+        <main>
+            <form className='input-container'>
+            <input className='input' type='text' placeholder='Top Text'></input>
+            <input className='input' type='text' placeholder='Bottom Text'></input>
+            <button className='button' onClick={handleClick}>Get a new meme image
             <img src={pictureIcon} alt='picture' className='button-icon'></img>
             </button>
-            </div>
-
-
-        </div>
+            </form>
+            
+        </main>
     )
 }
