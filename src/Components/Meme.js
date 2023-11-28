c6ffe08208a647090245ce329b090541e2994934
Shawnchee <aykaychee2@gmail.com>
2023-11-28 23:26:50 +0800

    small changes

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -2,16 +2,14 @@ import React from 'react'
 import pictureIcon from "./Icons/picture.svg"
 
 export default function Meme(){
-    function handleClick () {
-        alert('I was clicked')
-    }
+   
     return (
         <main>
             <form className='input-container'>
             <input className='input' type='text' placeholder='Top Text'></input>
             <input className='input' type='text' placeholder='Bottom Text'></input>
-            <button className='button' onClick={handleClick}>Get a new meme image
-            <img src={pictureIcon} alt='picture' className='button-icon'></img>
+            <button className='button'>Get a new meme image
+            <img src={pictureIcon}  alt='picture' className='button-icon'></img>
             </button>
             </form>
             
