5894f90e914731d0685eb5f7776f105dd4510b17
Shawnchee <aykaychee2@gmail.com>
2024-02-22 00:16:25 +0800

    added API

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import pictureIcon from "./Icons/picture.svg"
+import memesData from "../memesData.js"
 
 export default function Meme(){
+
+    let url
+
+    function getMemeImage(){
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+    }
    
     return (
         <main>
             <form className='input-container'>
             <input className='input' type='text' placeholder='Top Text'></input>
             <input className='input' type='text' placeholder='Bottom Text'></input>
-            <button className='button'>Get a new meme image
+            <button className='button'
+            onClick={getMemeImage}
+            >Get a new meme image
             <img src={pictureIcon}  alt='picture' className='button-icon'></img>
             </button>
             </form>
